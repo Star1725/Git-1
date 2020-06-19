513927892d4375f7ea93f4386a3f932fad45d407
Sergey <starovoytov1725@gmail.com>
2020-06-19 09:35:39 +0300

    created two java class

---
+++ src/ru/starovoytov1725/simpleProject/HelloWorld1.java
@@ -0,0 +1,10 @@
+package ru.starovoytov1725.simpleProject;
+
+/**
+ * Created by User on 019 19.06.20.
+ */
+public class HelloWorld1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, world1!");
+    }
+}
